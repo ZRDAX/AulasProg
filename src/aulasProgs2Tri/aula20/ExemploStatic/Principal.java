19de4728680925c504fc0b912348b0b9b2e55c5b
zrdax <xadrez90@outlook.com>
2024-06-21 16:29:39 -0300

    Heranças e polimorfismo

---
+++ src/aulasProgs2Tri/aula20/ExemploStatic/Principal.java
@@ -0,0 +1,21 @@
+package aulasProgs2Tri.aula20;
+
+public class Principal {
+    public static void main(String[] args) {
+        Conta c1 = new Conta("Ana", "123", 10);
+        System.out.println("Conta Cadastrada com ID "+c1.identificador);
+        Conta c2 = new Conta("josé", "345",100);
+        System.out.println("Conta cadastrada com ID "+c2.identificador);
+
+        //Testando o atributo estático
+
+        c1.nomeBanco = "CIMOL Bank";
+        System.out.println("Nome do banco c1 "+c1.nomeBanco);
+        System.out.println("Nome do banco c2 "+c2.nomeBanco);
+
+        //Acesso diretamente pela Classe, já que é um atributo estático
+        c2.nomeBanco = "INFO Bank";
+        System.out.println("Nome do banco c1 "+c1.nomeBanco);
+        System.out.println("Nome do banco c2 "+c2.nomeBanco);
+    }
+}
